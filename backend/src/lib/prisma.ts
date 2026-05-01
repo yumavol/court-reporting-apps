318028002be90d56c71840f46e977ad1e5724aa0
yumavol <yumavol@gmail.com>
2026-05-02 03:01:39 +0700

    feat: move dotenv configuration to prisma.ts for better environment variable management

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -2,6 +2,11 @@ import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@/generated/prisma/client';
 
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
